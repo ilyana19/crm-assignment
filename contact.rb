d1efb7a4be4ebe31633a9c17808e3988b4548421
ilyana19 <aries_2152@hotmail.com>
2018-07-25 12:49:57 -0400

    added the basic things from instruction

--- contact.rb
+++ contact.rb
@@ -1,28 +1,45 @@
 class Contact
+  @@contacts = []
+  @@id = 1
 
   # This method should initialize the contact's attributes
-  def initialize
-
+  def initialize(first_name, last_name, email, note = 'N/A')
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @id = @@id
+    @@id += 1 # this way the next contact will get a different id
   end
 
-  # This method should call the initializer, 
-  # store the newly created contact, and then return it
-  def self.create
+  # setting up atrribute accessors
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :email, :note
 
+  # This method should call the initializer,
+  # store the newly created contact, and then return it
+  def self.create(first_name, last_name, email, note)
+    new_contact = self.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
   end
 
   # This method should return all of the existing contacts
   def self.all
-
+    return @@contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
-
+  def self.find(id)
+    @@contacts.each do |contact|
+      if contact.id == id
+        return contact
+      end
+    end
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
@@ -40,11 +57,11 @@ class Contact
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    @@contacts.clear
   end
 
   def full_name
-
+    "#{@first_name} #{last_name}" # use readers if it exists, but can mix and match
   end
 
   # This method should delete the contact
@@ -54,5 +71,5 @@ class Contact
   end
 
   # Feel free to add other methods here, if you need them.
-  
+
 end
